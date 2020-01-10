e75981266c92b95142e3c9215b59f2235b63386c
toomoo0331 <33276284+toomoo0331@users.noreply.github.com>
2020-01-10 19:46:06 +0900

    panel change

---
+++ Tapiocago/Assets/_Scenes/Scripts/StopRotate.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopRotate : MonoBehaviour
+{
+    int i = 0;
+    bool flag = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flag)
+        {
+            if (i < 50)
+            {
+                transform.Rotate(new Vector3(0, 0, 25));
+            }
+            if(50<=i && i < 80)
+            {
+                transform.Rotate(new Vector3(0, 0, 10));
+            }
+            if (80 <= i)
+            {
+                transform.Rotate(new Vector3(0, 0, 5));
+            }
+            i += 1;
+        }
+        if (i >= 140)
+        {
+            i = 0;
+            flag = false;
+        }
+    }
+
+    public void onClickAct()
+    {
+
+        Debug.Log("タッチされたにゃー");
+        flag = true;
+
+    }
+}
